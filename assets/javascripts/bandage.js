3881d06d0807dd4b66a94271c15f12837adf1dd3
calamari <g.tavonius@gmail.com>
2013-04-04 18:57:10 +0200

    collecting browser and environment informations

--- assets/javascripts/bandage.js
+++ assets/javascripts/bandage.js
@@ -1,12 +1,51 @@
-(function(win) {
+(function(win, doc, navigator) {
   "use strict";
-  var defaultOnError = win.onerror;
+  var docElement = doc.documentElement,
+      body       = doc.getElementsByTagName('body')[0],
+      defaultOnError = win.onerror,
 
-  window.onerror = function(errorMessage, url, lineNum) {
-    console.log("bandage", arguments);
+      viewport;
+
+  function createViewport() {
+    var width = window.innerWidth || docElement.clientWidth || body.clientWidth,
+        height = window.innerHeight || docElement.clientHeight || body.clientHeight;
+    viewport = { width: width, height: height };
+  }
+
+  window.onerror = function(errorMessage, file, lineNum) {
+    createViewport();
+    var obj = {
+      time: new Date(),
+      error: {
+        message: errorMessage,
+        file: file,
+        lineNum: lineNum
+      },
+      environment: {
+        userLanguage: navigator.userLanguage,
+        // IE document mode
+        documentMode: document.documentMode,
+        // browser data
+        browserWidth: viewport.width,
+        browserHeight: viewport.height,
+        screenWidth: win.screen.width || 0,
+        screenHeight: win.screen.height || 0,
+        browser: navigator.appCodeName,
+        browserName: navigator.appName,
+        browserVersion: navigator.appVersion,
+        userAgent: navigator.userAgent,
+        cookieEnabled: navigator.cookieEnabled,
+        platform: navigator.platform
+      },
+      request: {
+        url: doc.location.href,
+        queryString: doc.location.searc
+      }
+    };
+    console.log("bandage", obj);
 
     if (defaultOnError) {
-      defaultOnError(errorMessage, url, lineNum);
+      defaultOnError(errorMessage, file, lineNum);
     }
   };
-}(window));
+}(window, document, navigator));
